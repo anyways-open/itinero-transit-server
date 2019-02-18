e8247d745240defa37903fe53d7e69fb408d1b09
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-02-18 14:00:08 +0000

    Docker deployment files

---
+++ deployment/update-docker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+NAME="transit-api"
+PORT=5000
+IMAGE="anywaysopen/itinero-transit-server"
+docker pull
+
+LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+RUNNING=`docker inspect --format "{{.Image}}" $NAME`
+echo "Latest:" $LATEST
+echo "Running:" $RUNNING
+
+if [ "$RUNNING" != "$LATEST" ];then
+    echo "restart $NAME"
+    docker stop $NAME
+    docker rm $NAME
+    docker run -d --name $NAME -p $PORT:5000 $IMAGE
+else
+  echo "$NAME up to date."
+fi
