125d0bc2958802fdf7fd2ababa24985a344f12c1
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-04-26 14:55:57 +0200

    Update to latest version

--- src/Itinero.Transit.Api/Logic/JourneyBuilder.cs
+++ src/Itinero.Transit.Api/Logic/JourneyBuilder.cs
@@ -34,6 +34,7 @@ namespace Itinero.Transit.Api.Logic
             {
                 return journeys;
             }
+
             var sortedByArrivalDesc = journeys.OrderBy(j => 0 - j.Time);
 
             var result = new List<Journey<TransferMetric>>();
@@ -75,77 +76,49 @@ namespace Itinero.Transit.Api.Logic
             }
 
             var snapshot = State.TransitDb.Latest;
-            var fromId = snapshot.FindStop(from);
-            var toId = snapshot.FindStop(to);
 
-
-
+            
+            
+            
+            WithTime<TransferMetric> calculator;
             if (departure == null)
             {
                 // Departure time is null
                 // We calculate one with a latest arrival scan search
-                var lasJ = snapshot.CalculateLatestDeparture
-                    (p, from, to, 
-                    arrival.Value - TimeSpan.FromDays(1), arrival.Value);
-                departure = arrival - p.SearchLengthCalculator(lasJ.Root.Time.FromUnixTime(),
-                                arrival.Value);
+                calculator = snapshot
+                    .SelectProfile(p)
+                    .SelectStops(from, to)
+                    .SelectTimeFrame(arrival.Value.AddDays(-1), arrival.Value);
+                // This will set the timeframe correctly
+                calculator
+                    .LatestDepartureJourney(tuple => tuple.journeyStart - p.SearchLengthCalculator(tuple.journeyStart, tuple.journeyEnd));
             }
-
-            /*
-             * The following line calculates an earliest arrival based on the departure time.
-             * The earliest arrival is transformed into a filter which will optimize the PCS later on
-             *
-             * Note that the departure time might be put a little later by this method
-             * Note that this method guarantees to return an arrival time, either the given one or one obeying the minimum search span as defined in the realProfile
-             * 
-             */
-            var (departureNotNull, arrivalNotNull, filter) = snapshot.FindArrivalTime(
-                p, departure.Value, arrival, fromId, toId);
-
-
-            var journeys = snapshot.CalculateJourneys(p, fromId, toId, departureNotNull, arrivalNotNull, filter);
-
-
-            return journeys;
-        }
-
-
-        /// <summary>
-        /// If no arrival time is given, performs EAS to calculate an estimated latest arrival time.
-        ///
-        /// Note that: if both dates are already known, both are returned unchanged. EAS will still be performed though for optimization the PCS in a later phase.
-        ///
-        /// If arrival date is not known, an arrival time is calculated based on the RealProfile preferences
-        /// One minute of padding before and after is added.
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        private static (DateTime deparute, DateTime arrival, IConnectionFilter filter)
-            FindArrivalTime(
-                this TransitDb.TransitDbSnapShot snapshot,
-                RealLifeProfile profile,
-                DateTime departure, DateTime? arrival,
-                LocationId from, LocationId to)
-        {
-            if (arrival != null)
+            else if(arrival == null)
             {
-                var easJ = snapshot.CalculateEarliestArrival(profile, from, to, departure, arrival.Value,
-                    out var filter);
-                return (easJ.Root.Time.FromUnixTime(),
-                    arrival.Value, filter);
+                calculator = snapshot
+                    .SelectProfile(p)
+                    .SelectStops(from, to)
+                    .SelectTimeFrame(departure.Value, departure.Value.AddDays(1));
+
+
+                // This will set the time frame correctly + install a filter
+                calculator.EarliestArrivalJourney(
+                    tuple => tuple.journeyStart + p.SearchLengthCalculator(tuple.journeyStart, tuple.journeyEnd));
+               
             }
             else
             {
-                var easJ = snapshot.CalculateEarliestArrival
-                (profile, from, to, departure, departure+TimeSpan.FromDays(1), out var filter,
-                    (start, end) => start + profile.SearchLengthCalculator(start, end));
-                // THe actual search end time is calculated based on the length of the earliest arrival journey
-                // and what the function in the profile makes from it
-                var depTime = easJ.Root.Time.FromUnixTime();
-                var endTime = depTime +
-                              profile.SearchLengthCalculator(easJ.Root.Time.FromUnixTime(), easJ.Time.FromUnixTime());
-                return (depTime, endTime, filter);
+                 calculator = snapshot
+                    .SelectProfile(p)
+                    .SelectStops(from, to)
+                    .SelectTimeFrame(departure.Value, arrival.Value);
+                // Perform isochrone to speed up 'all journeys'
+                calculator.IsochroneFrom();
+
             }
+
+
+            return calculator.AllJourneys();
         }
     }
 }
