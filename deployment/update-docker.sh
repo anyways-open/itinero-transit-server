869cfccf6bab001ade8a06670685c5cce0bc3ba8
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-05-15 13:00:55 +0200

    Better update script

--- deployment/update-docker.sh
+++ deployment/update-docker.sh
@@ -3,28 +3,43 @@
 
 
 set -e
-
+echo "Use with '-staging' as argument to use the staging build"
 NAME="transit-api"
 PORT=5001
-IMAGE="anywaysopen/itinero-transit-server"
+IMAGE="anywaysopen/itinero-transit-server$1"
 docker pull $IMAGE
+echo "Docker pull is done"
 
+if [ "$(docker ps -q -f name=$NAME)" ]
+then
+    echo "Does exist"
+    # Docker container does exist
+    LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+    RUNNING=`docker inspect --format "{{.Image}}" $NAME`
+    echo "Latest:" $LATEST
+    echo "Running:" $RUNNING
 
-LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
-RUNNING=`docker inspect --format "{{.Image}}" $NAME`
-echo "Latest:" $LATEST
-echo "Running:" $RUNNING
-
-
-if [ "$RUNNING" == "Error: No such object: transit-api" ]; then
-   docker rm $NAME
-   docker run -d --name $NAME -v /var/services/transit-api/logs:/var/app/logs -v /var/services/transit-api/db:/var/app/db -p $PORT:5000 $IMAGE
-elif [ "$RUNNING" != "$LATEST" ];then
-    echo "restart $NAME"
+    if [ "$RUNNING" == "$LATEST" ]
+    then
+        echo "Is up to date and running - nothing to do here"
+        exit
+    fi
+    
+    # Docker container is running, but not the latest version
     docker stop $NAME
-    docker rm $NAME
-    docker run -d --rm --name $NAME -v /var/services/transit-api/logs:/var/app/logs -v /var/services/transit-api/db:/var/app/db -p $PORT:5000 $IMAGE
-else
-  echo "$NAME up to date."
 fi
+echo "Fallen through"
+# At this point, docker might or might not be running
+# But we have to update and start it anyway
+
+if [ "$(docker ps -aq -f status=exited -f name=$NAME)" ]; then
+        # cleanup residual container
+        docker rm $NAME
+fi
+
+echo "Starting the docker image"
+docker run -d --rm --name $NAME -v /var/services/transit-api/logs:/var/app/logs -v /var/services/transit-api/db:/var/app/db -p $PORT:5000 $IMAGE
+
+
+
 git pull
