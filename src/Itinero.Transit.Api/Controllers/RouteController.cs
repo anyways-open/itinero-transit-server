8f438f88f9bd85ae64df2b9eaa52974f539b9282
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-07-04 09:27:50 +0200

    Only keep trains

--- src/Itinero.Transit.Api/Controllers/RouteController.cs
+++ src/Itinero.Transit.Api/Controllers/RouteController.cs
@@ -20,26 +20,9 @@ namespace Itinero.Transit.Api.Controllers
             string profileName
         )
         {
-            var _profile = State.GlobalState.OtherModeBuilder.GetOsmProfile(profileName);
 
-            var _routerDb = State.GlobalState.RouterDb;
 
-            var startPoint = _routerDb.Snap((float) fromLon, (float) fromLat);
-            var endPoint = _routerDb.Snap((float) toLon, (float) toLat);
-
-            if (startPoint.IsError || endPoint.IsError)
-            {
-                return BadRequest("Could not snap to nearest roads");
-            }
-
-            var route = _routerDb.Calculate(_profile, startPoint.Value, endPoint.Value);
-
-            if (route.IsError)
-            {
-                return BadRequest("Didn't find a route");
-            }
-
-            return route.Value.Shape.Select(coor => new Coordinate(coor.Latitude, coor.Longitude)).ToList();
+            return null;
         }
     }
 }
---
+++ src/Itinero.Transit.Api/Logic/RoutePlanner.cs
@@ -0,0 +1,35 @@
+using Itinero.LocalGeo;
+using Itinero.Profiles;
+
+namespace Itinero.Transit.Api.Logic
+{
+    public class RoutePlanner
+    {
+        public Route PlanRoute(
+            Profile p,
+            float fromLat,
+            float fromLon,
+            float toLat,
+            float toLon)
+        {
+            var _routerDb = State.GlobalState.RouterDb;
+
+            var startPoint = _routerDb.Snap((float) fromLon, (float) fromLat);
+            var endPoint = _routerDb.Snap((float) toLon, (float) toLat);
+
+            if (startPoint.IsError || endPoint.IsError)
+            {
+                return null;
+            }
+
+            var route = _routerDb.Calculate(p, startPoint.Value, endPoint.Value);
+
+            if (route.IsError)
+            {
+                return null;
+            }
+
+            return route.Value;
+        }
+    }
+}
