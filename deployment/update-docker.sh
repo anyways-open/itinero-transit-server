c57198a2c2a455250ccdaaa8189b2aef34359cd1
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-07-17 10:59:57 +0200

    Add force restart option to update-docker script

--- deployment/update-docker.sh
+++ deployment/update-docker.sh
@@ -11,6 +11,13 @@ IMAGE="anywaysopen/itinero-transit-server$1"
 docker pull $IMAGE
 echo "Docker pull is done for $NAME"
 
+if [ "$1" == "--force-restart" ]
+then
+    docker stop $NAME
+    docker rm $NAME
+    ./update-docker
+fi
+
 if [ "$(docker ps -q -f name=$NAME$)" ] # The dollar at the end forces the "end of line" in the regex, causing not to match prefixed names
 then
     echo "Does exist"
