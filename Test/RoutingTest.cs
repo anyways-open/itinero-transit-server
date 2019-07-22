c667dc87902c849ad2420fd4e57dacbc2ad1ed56
Pieter Vander Vennet <pietervdvn@gmail.com>
2019-07-22 10:18:01 +0200

    Fix routing regression test

--- Test/RoutingTest.cs
+++ Test/RoutingTest.cs
@@ -19,8 +19,9 @@ namespace Test
                 omb.Create("osm&profile=pedestrian&maxDistance=5000", null, null);
             var osm = (OsmTransferGenerator) cacher.Fallback;
             var route = osm.CreateRoute(
-                (50.865205, 4.35096799999999),
-                (51.3135271727844, 4.43265616893768), out var isEmpty, out var errMssg);
+                (50.865205, 4.35096799999999), // Herman teirlinck
+                (50.86034, 4.36170), // Brussel Noord...
+                out var isEmpty, out var errMssg);
             Assert.NotNull(route);
             Assert.False(isEmpty);
         }
